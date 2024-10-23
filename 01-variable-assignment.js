91f4a014d1d17780390e4519755179f102bfa796
Prathamesh Godase <135104006+prathameshgodase15@users.noreply.github.com>
2024-10-23 20:08:47 +0530

    01-variable assignment
    I have Following Basic Questions and Solved in JavaScript.
    Log your city name on console
    Log your best name on console
    Log your country name on console
    Log your school name on console
    Log your college name on console

---
+++ 01-variable-assignment.js
@@ -0,0 +1,20 @@
+// Log your city name on console
+var cityName = "Akole";
+console.log("City Name:",cityName);
+
+// Log your best name on console
+var BestFriend= "Sandesh";
+console.log("Best Friend:", BestFriend);
+
+// Log your country name on console
+var country="India";
+console.log("Country:", country);
+
+// Log your school name on console
+var school="Adarsh Madhyamik Vidyalay";
+console.log("School:",school);
+
+// Log your college name on console
+var college="Sangamner College Sangamner";
+console.log("College", college);
+
