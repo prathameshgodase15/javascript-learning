aaf83e77070a71f9d4acf59b4ebed828c56d21d5
Prathamesh Godase <135104006+prathameshgodase15@users.noreply.github.com>
2024-10-23 21:09:47 +0530

    01-variableAssign

---
+++ 01-variableAssign.js
@@ -0,0 +1,37 @@
+// Declare the variable myfullname 
+var myfullname;
+
+// Log the Variable myfullname value before initialization on console
+console.log("My Full Name", myfullname);
+
+// Initialize the myfullname variable with first and last name only;
+myfullname = "Prathamesh Godase"; 
+console.log("My Firts & Last Name:",myfullname); 
+
+// Update myfullname with your first name, middle name, and last name, and log the updated value.
+myfullname = "Prathamesh Sandip Godase"; // Update
+console.log("My Full Name:",myfullname); 
+
+// Declare and initialize a variable myLove with the specified value, and log it.
+var myLove = "I love only JS"; 
+console.log("Language:",myLove); 
+
+// Creating multiple variables for form controls as listed below
+   var firstName = "Prathamesh";
+   console.log("First Name:", firstName);
+   
+   var lastName = "Godase";
+   console.log("Last Name:",lastName);
+   
+   var age = 23;
+   console.log("Age:",age);
+        
+   var address = "Akole, Ahilyanagar";
+   console.log("Address:",address);
+     
+    pinCode = "422610";
+    console.log("Pin Code:", pinCode);
+       
+    isMarried = false;
+    console.log("Is Married:", isMarried);
+    
