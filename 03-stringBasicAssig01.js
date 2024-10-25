e0e4506011c622e1b4b7b8b4d0e4aadceb09af20
Prathamesh Godase <135104006+prathameshgodase15@users.noreply.github.com>
2024-10-25 10:15:24 +0530

    update 03-stringBasicAssig01.js

---
+++ 03-stringBasicAssig01.js
@@ -0,0 +1,16 @@
+// Please have the function with name-> stringBasics(), No arguments and no return is required
+
+function stringBasics(){
+    var company = "Google";
+    console.log(`My dream company is ${company}`);
+    var hobbyOne = "Playing Cricket";
+    var hobbyTwo = "Traveling";
+    var hobbyThree = "Listen Music";
+    console.log(`My hobbies are: ${hobbyOne}, ${hobbyTwo}, ${hobbyThree}`);
+    
+    var result = hobbyOne.length + hobbyTwo.length + hobbyThree.length ;
+    console.log(`Sum of Total number of characters that is available in hobbyOne, hobbyTwo and hobbyThree : ${result} `);
+    
+    
+}
+stringBasics();
