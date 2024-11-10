0b9d6a117af235ffc026d021e98169f211b0aa34
Prathamesh Godase <135104006+prathameshgodase15@users.noreply.github.com>
2024-11-10 22:06:41 +0530

    06-for-assignment01.js

---
+++ 06-for-assignment01.js
@@ -0,0 +1,44 @@
+//1. Print numbers from 5 to 15 by incrementing 1
+for (var i = 5; i <= 15; i++) {
+    console.log(i);
+}
+
+console.log(`============================================================`);
+
+
+//2. Print numbers from 50 to 40, decrementing by 1
+for (var i = 50; i >= 40; i--) {
+    console.log(i);
+}
+console.log(`============================================================`);
+
+//3. Find the first 15 odd numbers
+for (var i = 1; i<= 15; i=i+2) {
+    console.log(i);
+}
+console.log(`============================================================`);
+
+//4. Find the first 10 even numbers
+for (var i = 0; i<= 10; i=i+2) {
+    console.log(i);
+}
+console.log(`============================================================`);
+
+//5. Print table of 5 like  → 5   10  15   20   25   ……   50
+for(var i=5; i<=50; i=i+5){
+    console.log(i);  
+}
+console.log(`============================================================`);
+
+//6. Print table of 10 like 10 20  30  40  ……  100
+for(var i=10; i<=100; i=i+10){
+    console.log(i);
+    
+}
+console.log(`============================================================`);
+
+// print table of 10 in reverse order  like → 100  90  80  70  ……  10
+for(var i=100; i>=10; i=i-10){
+    console.log(i);
+    
+}
